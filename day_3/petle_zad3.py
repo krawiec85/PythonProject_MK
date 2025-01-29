c45bf797c1017e90d568155e2ef3859e3448328a
CSComarch <krawiec85@o2.pl>
2025-01-29 12:46:51 +0100

    [3 6]

--- day_3/petle_zad3.py
+++ day_3/petle_zad3.py
@@ -17,3 +17,32 @@ licznik = 0
 while licznik < 10:
     licznik += 1
     print('Komunikat 3 !!!')
+
+password = input('Podaj hasło')
+while password != 'secret':
+    password = input('Błędne hasło, podaj ponownie')
+print('Hasło prawidłowe')
+
+lista = []
+lista_int = []
+while True:
+    wej = input('Podaj liczbę')
+    # A string is numeric if all characters in the string are numeric
+    if not wej.isnumeric():
+        break
+    lista.append(wej)
+    lista_int.append(int(wej))
+print(lista)
+print(lista_int)
+
+my_list = [1, 5, 2, 3, 5, 4, 5, 6, 5]
+element_to_remove = 5
+while element_to_remove in my_list:
+    my_list.remove(element_to_remove)
+
+print(my_list) # [1, 2, 3, 4, 6]
+
+my_list = [1, 5, 2, 3, 5, 4, 5, 6, 5]
+print(dict.fromkeys(my_list))
+unique_numbers = list(dict.fromkeys(my_list))
+print(unique_numbers)
