0f9693e516a33bacc21a79816eaf31fa2c749b91
CSComarch <krawiec85@o2.pl>
2025-01-29 15:52:57 +0100

    [3 11]

---
+++ day_3/fun_1.py
@@ -0,0 +1,30 @@
+# funkcja - wykonuje fragment kodu w dowolnym momencie
+# funkcja musi być zadeklarowana przed wywołaniem
+
+a = 8
+b = 6
+
+
+# deklaracja funkcji
+def dodaj():  # funkcja bez argumentów
+    print(a + b)
+
+dodaj()  # wywołanie funkcji
+
+def dodaj2(a, b):  # funkcja z argumentami (obowiązkowe do przekazania)
+    print(a + b)
+
+dodaj2(67, 89) # wywołanie funkcji
+# TypeError: dodaj2() missing 2 required positional arguments: 'a' and 'b'
+input_a = input('Podaj pierwszy argument: ')
+input_b = input('Podaj drugi argument: ')
+dodaj2(int(input_a), int(input_b))  # przekazanie wartości zmiennych
+
+def odejmij(a, b, c=0):  # funkcja z opcjonalnym argumentem (domyślnie 0)
+    print(a - b - c)
+
+odejmij(c=9, b=8, a=10)
+odejmij(1, 2, c=9) # -10
+# argumenty pozycyjne muszą być podane przed argumentami kolejnościami !!!!
+
+print (dodaj())
